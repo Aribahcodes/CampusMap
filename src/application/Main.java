9fcf79e1835021a083085726df16fa5713ed6046
mariy <mariy@AetherOfAribah>
2025-11-10 22:28:30 +0530

    lo

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,10 +7,15 @@ import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) {
         WebView webView = new WebView();
         WebEngine engine = webView.getEngine();
+
+        engine.setUserAgent(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36"
+        );
 
         engine.load(getClass().getResource("/map.html").toExternalForm());
 
@@ -20,6 +25,8 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        System.setProperty("javafx.webview.renderer", "webview2");
+        launch(args);
     }
+
 }
